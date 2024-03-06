d61f90c66e97e350124b0185888f40f70f9ea8c4
Kevin Lobsinger <kevlobsinger@gmail.com>
2024-03-06 16:37:42 -0500

    Feat: now uses React router Link to for naviagation

--- portfolio/src/Routes/Landing/Landing.jsx
+++ portfolio/src/Routes/Landing/Landing.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import headshot from '../../Assets/headshot.png';
-import './Landing.css'
+import './Landing.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 export function Landing() {
   return (
@@ -16,13 +17,13 @@ export function Landing() {
               <Card.Text>
                 From Service Worker to Web Wizard: Leveraging years of customer service expertise, I transition into the realm of web development with a keen eye for user experiences. My journey from managing cash flows and team supervision to mastering full-stack technologies showcases a dedication to excellence and a unique blend of skills. Empathetic to user needs and driven by the thrill of problem-solving, I create web applications that are not just functional but truly alive.
               </Card.Text>
-              <a href="#portfolio" className="btn btn-primary me-2">View My Work</a>
-              <a href="#contact" className="btn btn-outline-primary me-2">Let's Talk!</a>
+              <Link to="/Portfolio" className="btn btn-primary me-2">View My Work</Link>
+              <Link to="/Contact" className="btn btn-outline-primary me-2">Let's Talk!</Link>
               <a href="https://github.com/Klobsinger" className="logo-btn me-2 github" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faGithub} size="2x" />
+                <FontAwesomeIcon icon={faGithub} size="2x" />
               </a>
               <a href="https://linkedin.com/in/kevin-lobsinger" className="logo-btn me-2" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
               </a>
             </Card.Body>
           </Card>
