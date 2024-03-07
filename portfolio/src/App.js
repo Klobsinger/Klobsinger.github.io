554c512837c0091389577166270070dba059ae16
Kevin Lobsinger <kevlobsinger@gmail.com>
2024-03-06 21:00:13 -0500

    Add: routes for website sections

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { NavBar } from './Routes/NavBar/NavBar';
-import { Footer } from './Routes/Footer/Footer'
-import { Landing } from './Routes/Landing/Landing'
+import { Footer } from './Routes/Footer/Footer';
+import { Landing } from './Routes/Landing/Landing';
+import { About } from './Routes/About/About';
+import { Contact } from './Routes/Contact/Contact';
+import { Portfolio } from './Routes/Portfolio/Portfolio';
 
 function App() {
   return (
     <>
-    <NavBar />
-   <Routes>
-   <Route path="/" element={<Landing />} />
-   </Routes>
-   <Footer />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        
+       
+        
+    
+      </Routes>
+      <Footer />
     </>
   );
 }
--- portfolio/src/Routes/NavBar/NavBar.jsx
+++ portfolio/src/Routes/NavBar/NavBar.jsx
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import "./NavBar.css"
+import { NavLink } from 'react-router-dom';
 
 export function NavBar() {
   return (
@@ -10,13 +11,13 @@ export function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
-            <Nav.Link href="#link">Projects</Nav.Link>
-            <Nav.Link href="#link">Contact me</Nav.Link>
+          <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+          <Nav.Link as={NavLink} to="/About">About</Nav.Link>
+          <Nav.Link as={NavLink} to="/Portfolio">Projects</Nav.Link>
+          <Nav.Link as={NavLink} to="/Contact">Contact me</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+} 
