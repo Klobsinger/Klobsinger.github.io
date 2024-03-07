8810458295ad8e07a739163ce38661036eddc6e8
Kevin Lobsinger <kevlobsinger@gmail.com>
2024-03-06 21:00:57 -0500

    Feat: update message

--- portfolio/src/Routes/Landing/Landing.jsx
+++ portfolio/src/Routes/Landing/Landing.jsx
@@ -12,13 +12,14 @@ export function Landing() {
       <Row className="my-5 align-items-center">
         <Col md={6}>
           <Card style={{ border: 'none', backgroundColor: 'transparent' }}>
-            <Card.Body>
-              <Card.Title>Hi, I'm Kevin. I Make Websites Come Alive</Card.Title>
+            <Card.Body className="custom-card-body">
+              <Card.Title>Hi, I'm Kevin.</Card.Title>
               <Card.Text>
-                From Service Worker to Web Wizard: Leveraging years of customer service expertise, I transition into the realm of web development with a keen eye for user experiences. My journey from managing cash flows and team supervision to mastering full-stack technologies showcases a dedication to excellence and a unique blend of skills. Empathetic to user needs and driven by the thrill of problem-solving, I create web applications that are not just functional but truly alive.
+              where creativity meets code. Transitioning from customer service to the dynamic world of web development, I blend empathy with technology to craft websites that breathe life into user experiences. Discover the magic of my full-stack development journey and how I turn challenges into digital solutions.
               </Card.Text>
               <Link to="/Portfolio" className="btn btn-primary me-2">View My Work</Link>
               <Link to="/Contact" className="btn btn-outline-primary me-2">Let's Talk!</Link>
+              <Link to="/About" className="btn btn-outline-primary me-2">More About me</Link>
               <a href="https://github.com/Klobsinger" className="logo-btn me-2 github" target="_blank" rel="noopener noreferrer">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
               </a>
@@ -29,7 +30,7 @@ export function Landing() {
           </Card>
         </Col>
         <Col md={6}>
-          <img src={headshot} alt="Kevin" className="img-fluid" />
+          <img src={headshot} alt="Kevin" className="img-fluid headshot" />
         </Col>
       </Row>
     </Container>
